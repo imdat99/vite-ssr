426d63867c7d980c3ed50c3bc3f302b59c342eff
imdat99 <imdat2999@gmail.com>
2024-03-29 01:47:00 +0700

    chore: init

---
+++ src/About.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+const About: React.FC<AboutProps> = (props) => {
+
+    return (
+        <>
+            <h1>About Us</h1>
+        </>
+    )
+}
+
+interface AboutProps {
+    [key: string]: any
+}
+
+export default About;
